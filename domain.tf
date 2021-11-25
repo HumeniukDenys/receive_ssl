dd8f0999de269f536d81f3d5760925219061ad60
HumeniukDenys <83279539+HumeniukDenys@users.noreply.github.com>
2021-11-26 00:51:58 +0200

    remove domain

--- domain.tf
+++ domain.tf
@@ -9,9 +9,3 @@ module "testfortvppua" {
   source        = "./modules/get_ssl"
   email_address = "admin@fortv.pp.ua"
 }
-
-module "bau000com" {
-  common_name   = "bau000.com"
-  source        = "./modules/get_ssl"
-  email_address = "admin@bau000.com"
-}
