b853f6af579d6e18f5d90103d570ff6223215bfb
HumeniukDenys <83279539+HumeniukDenys@users.noreply.github.com>
2022-01-05 01:31:41 +0200

    Update main.tf

--- modules/get_ssl/main.tf
+++ modules/get_ssl/main.tf
@@ -2,19 +2,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "receive-ssl"
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-        }
-      }
-    }
+backend "s3" {
+    bucket = "receive-ssl"
+    key    = "./terraform.tfstate"
+    region = "eu-central-1"
 }
 
 provider "acme" {
