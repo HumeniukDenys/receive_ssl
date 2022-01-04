16c08bddd991b248673149b76f0c2a79c943bef6
HumeniukDenys <83279539+HumeniukDenys@users.noreply.github.com>
2022-01-04 23:48:32 +0200

    Update main.tf

--- modules/get_ssl/main.tf
+++ modules/get_ssl/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 provider "acme" {
   #server_url = "https://acme-v02.api.letsencrypt.org/directory"
   server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
