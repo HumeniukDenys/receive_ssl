2ea1f80a4e2bdea2e038fc96643a3432f7b50be1
HumeniukDenys <83279539+HumeniukDenys@users.noreply.github.com>
2022-02-03 00:15:38 +0200

    Update versions.tf

--- modules/get_ssl/versions.tf
+++ modules/get_ssl/versions.tf
@@ -24,10 +24,6 @@ terraform {
   required_version = ">= 0.15"
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
-
 provider "acme" {
   #server_url = "https://acme-v02.api.letsencrypt.org/directory"
   server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
