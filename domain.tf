0f60b96250104538ea32b35d575e11bdb01f200b
AIRpwnz <air@uhost.ovh>
2022-02-03 11:47:21 +0200

    Add Fortv.pp.ua domain for issue ssl

--- domain.tf
+++ domain.tf
@@ -3,15 +3,3 @@ module "fortvppua" {
   source        = "./modules/get_ssl"
   email_address = "admin@fortv.pp.ua"
 }
-
-module "testfortvppua" {
-  common_name   = "test.fortv.pp.ua"
-  source        = "./modules/get_ssl"
-  email_address = "admin@fortv.pp.ua"
-}
-
-module "newfortvppua" {
-  common_name   = "new.fortv.pp.ua"
-  source        = "./modules/get_ssl"
-  email_address = "admin@fortv.pp.ua"
-}
