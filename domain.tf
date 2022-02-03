2e768f19cde507e57742001b059405a8d9183b31
HumeniukDenys <83279539+HumeniukDenys@users.noreply.github.com>
2022-02-03 16:15:40 +0200

    Update domain.tf

--- domain.tf
+++ domain.tf
@@ -4,3 +4,8 @@ module "fortvppua" {
   email_address = "admin@fortv.pp.ua"
 }
 
+module "afortvppua" {
+  common_name   = "a.fortv.pp.ua"
+  source        = "./modules/get_ssl"
+  email_address = "admin@fortv.pp.ua"
+}
